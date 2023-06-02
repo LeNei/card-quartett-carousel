bf7fafe88fa082003a1732d11b6c2390368a4ee8
LeNei <leonmarc.neisskenwirth@gmail.com>
2023-06-02 22:57:11 +0200

    center content

--- src/routes/car/[slug]/+layout.svelte
+++ src/routes/car/[slug]/+layout.svelte
@@ -1,4 +1,4 @@
-<div class="min-h-screen w-full flex items-center bg-zinc-900">
+<div class="min-h-screen w-full flex justify-center bg-zinc-900">
 	<div class="max-w-3xl w-full p-5 text-white">
 		<slot />
 	</div>
